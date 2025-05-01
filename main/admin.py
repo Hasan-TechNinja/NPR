416c7ab552153571742ff63651909080a51af37a
Hasan-TechNinja <hasantechninaj@gmail.com>
2025-05-01 11:41:44 +0600

    product details complete

--- main/admin.py
+++ main/admin.py
@@ -5,28 +5,28 @@ from . models import Brand, Category, Product, Review, Visitor
 
 class BrandAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'user', 'name', 'description', 'image', 'created'
+        'id', 'user', 'name', 'description', 'image', 'created', 'active'
     )
 admin.site.register(Brand, BrandAdmin)
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'user', 'name', 'description', 'image', 'created',
+        'id', 'user', 'name', 'description', 'image', 'created', 'active'
     )
 admin.site.register(Category, CategoryAdmin)
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'user', 'name', 'brand', 'category', 'description', 'image', 'rating', 'price'
+        'id', 'user', 'name', 'brand', 'category', 'description', 'image', 'rating', 'price', 'active'
     )
 admin.site.register(Product, ProductAdmin)
 
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'product', 'review', 'rating', 'vote_score', 'visitor', 'created'
+        'id', 'product', 'review', 'rating', 'vote_score', 'visitor', 'created', 'active'
     )
 admin.site.register(Review, ReviewAdmin)
 
